76899dcdbe55178bc41c06349a982783960f8f0e
fumani13 <fl.makhubela13@gmail.com>
2020-07-23 23:09:03 +0200

    adding list screen

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import {EmployeeService} from '../shared/employee.service';
-
+ 
+import { EmployeeService } from '../shared/employee.service'
+import { Employee } from '../shared/employee.model';    
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-
-  constructor(private employeeService : EmployeeService) { }
-
+ 
+  constructor(private employeeService: EmployeeService) { }
+ 
   ngOnInit() {
+    this.employeeService.getEmployeeList();
   }
-
+ 
+  showForEdit(emp: Employee) {
+    this.employeeService.selectedEmployee = Object.assign({}, emp);;
+  }
+ 
+ 
+  onDelete(id: number) {
+    if (confirm('Are you sure to delete this record ?') == true) {
+      this.employeeService.deleteEmployee(id)
+      .subscribe(x => {
+        this.employeeService.getEmployeeList();
+      })
+    }
+  }
 }
